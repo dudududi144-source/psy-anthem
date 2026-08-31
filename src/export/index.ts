5c14a57425d18f23ddc05f35d97bef69fba4c28c
dudududu144 <dudududi144@gmail.com>
2026-09-01 02:43:54 +0300

    feat: MIDI export (SMF format 1) - real DAW output for MusicalEvent[]

---
+++ src/export/index.ts
@@ -0,0 +1,10 @@
+// PSY ANTHEM - export/index.ts
+export {
+  toMidi,
+  writeMidiFile,
+  encodeVarLen,
+  DEFAULT_DIVISION,
+  DEFAULT_PROGRAMS,
+  DEFAULT_TRACK_NAMES,
+} from './midi';
+export type { MidiEncodeOptions } from './midi';
