7454a2433889e73dc6e8b3c03ad7572d8b12423a
dudududu144 <dudududi144@gmail.com>
2026-09-01 03:16:18 +0300

    feat: advanced quality system - A/B testing framework

---
+++ src/testing/index.ts
@@ -0,0 +1,14 @@
+// PSY ANTHEM - testing/index.ts
+export {
+  standardAlgorithm,
+  strictLeapRecoveryAlgorithm,
+  compareAlgorithms,
+  analyzeParameterSensitivity,
+} from './ab-test';
+export type {
+  AlgorithmFn,
+  ComparisonReport,
+  SensitivityRow,
+  SensitivityReport,
+  SensitivityParam,
+} from './ab-test';
